438ca40552cc6e3749d2b723c436cab77e7d74ab
RobertFauland <robertfauland@gmx.at>
2022-06-17 00:16:17 +0200

    test

--- app/src/main/java/com/example/myApp/screens/game/Gamescreen.kt
+++ app/src/main/java/com/example/myApp/screens/game/Gamescreen.kt
@@ -5,45 +5,30 @@ package com.example.myApp.screens.game
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.icu.text.LocaleDisplayNames
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.input.pointer.consumeAllChanges
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.myApp.R
 import org.burnoutcrew.reorderable.ReorderableItem
-import org.burnoutcrew.reorderable.detectReorderAfterLongPress
+import org.burnoutcrew.reorderable.detectReorder
 import org.burnoutcrew.reorderable.rememberReorderableLazyGridState
 import org.burnoutcrew.reorderable.reorderable
-import java.util.*
-import kotlin.math.roundToInt
 
 
 
@@ -70,127 +55,126 @@ fun Gamescreen (navController: NavController = rememberNavController(), difficul
             }
         }
     ) {
-        //MainContent(difficulty, imageID)
-        VerticalGrid()
+        MainContent(difficulty, imageID, navController)
     }
 }
-
-
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@OptIn(ExperimentalFoundationApi::class)
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "UnrememberedMutableState")
 @Composable
-fun MainContent(difficulty: Int, imageID: Int) {
-    var colum = 3 //Leicht
-    if (difficulty == 1)  { //Mittel
+fun MainContent(difficulty: Int, imageID: Int, navController: NavController) {
+    var colum = 3 //easy
+    if (difficulty == 1)  { //normal
         colum = 4
-    } else if (difficulty == 2) { //Schwer
+    } else if (difficulty == 2) { //hard
         colum = 5
     }
-    val list = (1..colum*colum).map { it.toString() } //9,16,25
-    val configuration = LocalConfiguration.current
-    val imageHeight = (configuration.screenHeightDp.dp - (101-(5-colum)).dp) / colum
-
     val b: Bitmap = BitmapFactory.decodeResource(LocalContext.current.resources, R.drawable.rosen)
     val splittedBitmap = splitImage(b,colum*colum)
-    //3 = 99, 4 = 100, 5 = 101
+    val data = remember {mutableStateOf(List(colum*colum) { it })} //Index of Grid fields, ordered
+
+    val state = rememberReorderableLazyGridState(onMove = {from, to ->
+        data.value = data.value.toMutableList().apply {
+            swap(to.index, from.index)
+        }
+    })
+
+    if (isGameOver(data.value) && statik.count == 0) {// If Sorted
+            data.value = data.value.toMutableList().apply { //Randomise
+                shuffle()
+            }
+    }
+
+    statik() //For Closing Game
+
+    if (isGameOver(data.value) && statik.count > 0) {
+        popUpWindow(navController)
+    }
+
     Scaffold(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
             .padding(5.dp),
     ) {
-
         LazyVerticalGrid(
-            columns = GridCells.Fixed(colum), //Anzahl der Spalten
-            modifier = Modifier,
-            contentPadding = PaddingValues(
-                start = 12.dp,
-                top = 16.dp,
-                end = 12.dp,
-                bottom = 16.dp
-            )
-        ){
-            itemsIndexed(list) { it, list ->
-                Card(
-                    modifier = Modifier
-                        .padding(1.dp)
-                        .fillMaxWidth()
-                        .clickable {
-                            move(splittedBitmap, it)
-                        }
-                        .fillMaxHeight(),
-                    elevation = 8.dp,
-                ) {
-                    Image(bitmap = splittedBitmap[it].asImageBitmap(), contentDescription = "Rosen")
+            columns = GridCells.Fixed(colum),
+            state = state.gridState,
+            contentPadding = PaddingValues(horizontal = 2.dp),
+            verticalArrangement = Arrangement.spacedBy(1.dp),
+            horizontalArrangement = Arrangement.spacedBy(1.dp),
+            modifier = Modifier
+                .reorderable(state)
+                .detectReorder(state) //hold on the grid element for Movement
+        ) {
+            items(data.value, { it }) { item ->
+                ReorderableItem(state, key = item, defaultDraggingModifier = Modifier) {
+                   Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                    ) {
+                        Image(
+                            bitmap = splittedBitmap[item].asImageBitmap(),
+                            contentDescription = "Rosen",
+                            modifier = Modifier.detectReorder(state)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-private fun move(splittedBitmap:ArrayList<Bitmap>,imgID:Int) {
-    Collections.swap(splittedBitmap, imgID, 6)
-}
-
 @Composable
-private fun VerticalGrid(){
-    val data = remember {mutableStateOf(List(100) { "item $it" })}
-    val state = rememberReorderableLazyGridState(onMove = {from, to ->
-        data.value = data.value.toMutableList().apply {
-            add(to.index, removeAt(from.index))
+fun popUpWindow(navController: NavController){
+    val open = remember { mutableStateOf(false)}
+
+    AlertDialog(
+        onDismissRequest = {
+        open.value = false
+    }, title = {
+        Text(text = "GEWONNEN!")
+    },
+        confirmButton = {
+        Button(
+            onClick = {
+                open.value = true
+                navController.navigate(route = "homescreen")
+            }) {
+            Text("OK")
         }
-    })
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(4),
-        state = state.gridState,
-        contentPadding = PaddingValues(horizontal = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        modifier = Modifier
-            .reorderable(state)
-            .detectReorderAfterLongPress(state)
-    ) {
-        items(data.value, { it }) { item ->
-            ReorderableItem(state, key = item, defaultDraggingModifier = Modifier) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .aspectRatio(1f)
-                        .size(100.dp)
-                ) {
-                    Text(text = item,
-                        modifier = Modifier.detectReorderAfterLongPress(state))
-                }
+    },
+        dismissButton = {
+            Button(
+                onClick = {
+                    open.value = true
+                }) {
+                Text("Cancel")
+
             }
         }
-    }
-}
-
-
-@Composable
-fun movePictures(pic1: Int, pic2: Int, listItem: LocaleDisplayNames.UiListItem) {
-    val offsetX = remember { mutableStateOf( 0f)}
-    val offsetY = remember { mutableStateOf( 0f)}
-
-    Box(
-        modifier = Modifier
-            .offset {
-                IntOffset(
-                    x = offsetX.value.roundToInt(),
-                    y = offsetY.value.roundToInt()
-                )
-            }
-            .pointerInput(Unit) {
-                detectDragGestures { change, dragAmount ->
-                    change.consumeAllChanges()
-                    offsetX.value += dragAmount.x
-                    offsetY.value += dragAmount.y
-                }
-            }
-            .size(80.dp)
-            .clip(CircleShape)
-            .background(Color.Blue),
     )
+}
+
+class statik() {
+    companion object {
+        var count:Int=0;
+    }
+    init {
+        count++
+    }
+}
+
+fun isGameOver(data: List<Int>): Boolean {
+    if (data == data.toMutableList().sorted()) {// If Sorted
+        return true
+    }
+    return false
+}
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int){
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
 }
 
 fun splitImage(image: Bitmap, chunkNumbers: Int): ArrayList<Bitmap>{
