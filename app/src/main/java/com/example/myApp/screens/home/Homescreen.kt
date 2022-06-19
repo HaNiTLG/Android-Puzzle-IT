0d2caca0f3db330a7fd18b1c5288c335290145b3
HaNiTLG <admin@hanitlg.de>
2022-06-19 17:18:46 +0200

    Removed Favoritscreen
    Favoritscreen has been removed.

--- app/src/main/java/com/example/myApp/screens/home/Homescreen.kt
+++ app/src/main/java/com/example/myApp/screens/home/Homescreen.kt
@@ -3,19 +3,13 @@ package com.example.myApp.screens.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -34,32 +28,6 @@ fun HomeScreen(navController: NavController = rememberNavController()){
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(text = "Main Menu", style = MaterialTheme.typography.caption) },
-                actions = {
-                    IconButton(onClick = { showMenu = !showMenu }) {
-                        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More")
-                    }
-
-                    DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false}) {
-                        DropdownMenuItem(onClick = { /*TODO*/ }) {
-                            Row(modifier = Modifier
-                                .clickable { navController.navigate(route = "Level3")}
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Favorite,
-                                    contentDescription = "Favorites",
-                                    modifier = Modifier
-                                        .padding(4.dp)
-                                )
-                                Text(
-                                    text = "Favorites",
-                                    modifier = Modifier
-                                        .padding(4.dp)
-                                        .width(100.dp)
-                                )
-                            }
-                        }
-                    }
-                }
             )
         }
     ) {
@@ -67,7 +35,6 @@ fun HomeScreen(navController: NavController = rememberNavController()){
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainContent(puzzles: List<PuzzleList> = getPuzzles(), navController: NavController) {
     LazyVerticalGrid (
